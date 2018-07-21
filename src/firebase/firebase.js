cd4179f572df6f0b409210f5f8aa734968c7b91a
Zi <ziyingliuu@gmail.com>
2018-07-20 18:11:36 -0700

    firebase configs

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyDEvmze0TnUUZRWwvrbz9dt2aUGsIAjeu4",
+  authDomain: "suggestionsboard-2d9ab.firebaseapp.com",
+  databaseURL: "https://suggestionsboard-2d9ab.firebaseio.com",
+  projectId: "suggestionsboard-2d9ab",
+  storageBucket: "suggestionsboard-2d9ab.appspot.com",
+  messagingSenderId: "812077124491"
+};
+
+const auth = firebase.auth();
+
+export {
+  auth
+};
